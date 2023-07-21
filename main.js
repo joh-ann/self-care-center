0c9280f6e96286f4b063721213710532a30c9a90
Johann Dee <johanndee1994@gmail.com>
2023-07-20 20:17:50 -0700

    Add load, elements, eventlisteners

---
+++ main.js
@@ -0,0 +1,32 @@
+// load
+document.addEventListener("DOMContentLoaded", function() {
+
+});
+
+
+// elements
+var logo = document.getElementById("logo");
+var affirmBtn = document.getElementById("aff");
+var mantraBtn = document.getElementById("man");
+var receiveMsg = document.querySelector(".button")
+var medImg = document.querySelector("medImg")
+
+
+
+// event listeners
+logo.addEventListener("click", function () {
+    location.reload();
+});
+
+affirmBtn.addEventListener("click", function() {
+
+});
+
+mantraBtn.addEventListener("click", function() {
+
+});
+
+receiveMsg.addEventListener("click", function() {
+
+});
+
