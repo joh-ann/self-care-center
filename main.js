4e1fc62ee30f6a5a28ca85ba87621874f1af90c1
Johann Dee <johanndee1994@gmail.com>
2023-07-20 22:31:41 -0700

    Revert back to no login

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 // load
 document.addEventListener("DOMContentLoaded", function() {
-    showLogin();
+
 });
 
 
@@ -12,9 +12,6 @@ var mantraBtn = document.getElementById("man");
 var receiveMsg = document.querySelector(".button");
 var medImg = document.querySelector("medImg");
 var container2 = document.querySelector(".container2");
-var mainContainer = document.querySelector(".main-container");
-var loginContainer = document.querySelector(".login-container");
-var loginForm = document.getElementById("login-form");
 
 // event listeners
 // reload home page on click
@@ -22,21 +19,19 @@ logo.addEventListener("click", function () {
     location.reload();
 });
 
-// uncheck mantra
 affirmBtn.addEventListener("click", function () {
     if (affirmBtn.checked) {
         mantraBtn.checked = false;
     }
 });
 
-// uncheck affirm
+
 mantraBtn.addEventListener("click", function () {
     if (mantraBtn.checked) {
         affirmBtn.checked = false;
     }
 });
 
-// display either affirm or mantra
 receiveMsg.addEventListener("click", function () {
     if (affirmBtn.checked) {
       displayRandomMsg(affirmations);
@@ -45,17 +40,6 @@ receiveMsg.addEventListener("click", function () {
     }
   });
 
-loginForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    // get user name from input
-    var nameInput = document.getElementById("name");
-    var userName = nameInput.value;
-
-    showMain(userName);
-});
-
-
 // functions
 function displayRandomMsg(messages) {
     var randomIndex = Math.floor(Math.random() * messages.length);
@@ -63,19 +47,3 @@ function displayRandomMsg(messages) {
 
     container2.innerHTML = `<p>${message}</p>`
 }
-
-function showLogin() {
-    mainContainer.classList.add("hide");
-    loginContainer.classList.remove("hide");
-}
-
-function showMain(userName) {
-    var greeting = document.createElement("h2");
-    greeting.textContent = `Hello, ${userName}! Welcome to the Self Care Center.`;
-    document.querySelector(".greeting").appendChild(greeting);
-
-    loginContainer.classList.add("hide");
-    mainContainer.classList.remove("hide");
-}
-
-
