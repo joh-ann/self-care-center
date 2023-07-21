a490ab0a36637d93d21438dbe961fdf6b376db1e
Johann Dee <johanndee1994@gmail.com>
2023-07-20 20:50:59 -0700

    Add displayRandomMsg function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // load
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -8,25 +9,37 @@ document.addEventListener("DOMContentLoaded", function() {
 var logo = document.getElementById("logo");
 var affirmBtn = document.getElementById("aff");
 var mantraBtn = document.getElementById("man");
-var receiveMsg = document.querySelector(".button")
-var medImg = document.querySelector("medImg")
-
-
+var receiveMsg = document.querySelector(".button");
+var medImg = document.querySelector("medImg");
+var container2 = document.querySelector(".container2");
 
 // event listeners
+// reload home page on click
 logo.addEventListener("click", function () {
     location.reload();
 });
 
-affirmBtn.addEventListener("click", function() {
+affirmBtn.addEventListener("click", function () {
 
 });
 
-mantraBtn.addEventListener("click", function() {
+
+mantraBtn.addEventListener("click", function () {
 
 });
 
-receiveMsg.addEventListener("click", function() {
+receiveMsg.addEventListener("click", function () {
+    if (affirmBtn.checked) {
+      displayRandomMsg(affirmations);
+    } else if (mantraBtn.checked) {
+      displayRandomMsg(mantras);
+    }
+  });
 
-});
+// functions
+function displayRandomMsg(messages) {
+    var randomIndex = Math.floor(Math.random() * messages.length);
+    var message = messages[randomIndex]
 
+    container2.innerHTML = `<p>${message}</p>`
+}
